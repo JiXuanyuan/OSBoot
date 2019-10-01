97bfcdaf625831145095fbc90d0371e157b0524d
陈佳辉 <chenjiahui@chenjiahuideMacBook-Air.local>
2019-10-02 02:15:59 +0800

    增加软盘启动程序

---
+++ section03/bootsect.s
@@ -0,0 +1,42 @@
+org 0x7c00
+;global _start
+
+[section .data]
+
+
+[section .text]
+
+_start:
+	mov ax, 0
+	mov ss, ax
+	mov sp, _stack_len
+	mov ds, ax
+	mov es, ax
+
+	mov si, _msg
+	mov cx, _msg_len
+
+_print_msg:
+	mov al, [ds:si]
+	inc si
+
+	mov ah, 0x0e
+	mov bx, 15
+	int 0x10
+	loop _print_msg
+
+_fin:
+	hlt
+	jmp _fin
+
+_msg:
+	db 0x0a, "Hello, world!", 0x0a
+	_msg_len equ ($ - _msg)
+
+_stack:
+	times 128 db 0
+	_stack_len equ ($ - _stack)
+
+_blank:
+	times 0x1fe-($-$$) db 0
+	db 0x55, 0xaa
